28100fbb20a880fb84f00a857647bf977747088e
Felipe Jung <felipemattoseu@gmail.com>
2025-04-05 09:53:15 -0400

    Add gluestack ui

--- App.tsx
+++ App.tsx
@@ -1,4 +1,8 @@
-import { View, Text } from "react-native";
+import "@/global.css";
+
+import { View } from "react-native";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { Text } from "@/components/ui/text";
 import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
 import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
@@ -10,21 +14,26 @@ export default function App() {
   });
 
   if (!fontsLoaded) {
-    return <View />;
+    return (
+      <GluestackUIProvider mode="light">
+        <View />
+      </GluestackUIProvider>
+    );
   }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#202024",
-      }}
-    >
-      <StatusBar style="light" />
-
-      <Text>Home</Text>
-    </View>
+    <GluestackUIProvider mode="light">
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#202024",
+        }}
+      >
+        <StatusBar style="light" />
+        <Text>Home</Text>
+      </View>
+    </GluestackUIProvider>
   );
 }
