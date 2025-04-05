eef3997ed34b8d98b32e0863bb23b9ca89c64e3c
Felipe Jung <felipemattoseu@gmail.com>
2025-04-05 14:04:46 -0400

    Add Exercise, Home, and Profile screens with basic layout; update package dependencies to include @react-navigation/bottom-tabs for navigation support.

---
+++ screens/exercise.tsx
@@ -0,0 +1,10 @@
+import { Center } from "@/components/ui/center";
+import { Text } from "@/components/ui/text";
+
+export function Exercise() {
+  return (
+    <Center className="flex-1 bg-brand-gray-900">
+      <Text>Exercise</Text>
+    </Center>
+  );
+}
---
+++ screens/home.tsx
@@ -0,0 +1,10 @@
+import { Center } from "@/components/ui/center";
+import { Text } from "@/components/ui/text";
+
+export function Home() {
+  return (
+    <Center className="flex-1 bg-brand-gray-900">
+      <Text>Home</Text>
+    </Center>
+  );
+}
