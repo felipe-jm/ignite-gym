77c645557503d87daf8c4f50fe9f413350d927ba
Felipe Jung <felipemattoseu@gmail.com>
2025-04-05 14:56:24 -0400

    Add exercise section to Home screen; include Heading and Text components to display the number of groups. Update layout with VStack and HStack for improved UI structure.

--- screens/home.tsx
+++ screens/home.tsx
@@ -4,6 +4,9 @@ import { FlatList } from "react-native";
 import { HomeHeader } from "@/components/home-header";
 import { VStack } from "@/components/ui/vstack";
 import { Group } from "@/components/group";
+import { HStack } from "@/components/ui/hstack";
+import { Heading } from "@/components/ui/heading";
+import { Text } from "@/components/ui/text";
 
 export function Home() {
   const [groups, setGroups] = useState<string[]>([
@@ -34,6 +37,18 @@ export function Home() {
         contentContainerStyle={{ paddingHorizontal: 24 }}
         style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
       />
+
+      <VStack className="flex-1 px-8">
+        <HStack className="flex-1 gap-2 items-center justify-between">
+          <Heading className="text-gray-200 font-bold text-base">
+            Exercícios
+          </Heading>
+
+          <Text className="text-gray-200 font-regular text-sm">
+            {groups.length}
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
