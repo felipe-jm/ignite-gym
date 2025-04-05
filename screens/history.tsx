51f07ac6dde96253b248ef59a323d24d4c3a9bd7
Felipe Jung <felipemattoseu@gmail.com>
2025-04-05 15:26:52 -0400

    Refactor History screen to utilize SectionList for displaying exercises; implement state management for exercises and enhance UI with dynamic headers and empty state handling.

--- screens/history.tsx
+++ screens/history.tsx
@@ -1,12 +1,43 @@
+import { useState } from "react";
+import { SectionList } from "react-native";
+
 import { VStack } from "@/components/ui/vstack";
 import { ScreenHeader } from "@/components/screen-header";
 import { HistoryCard } from "@/components/history-card";
+import { Heading } from "@/components/ui/heading";
+
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: "Costa",
+      data: ["Puxada frontal", "Remada"],
+    },
+    {
+      title: "Peito",
+      data: ["Supino", "Supino inclinado"],
+    },
+  ]);
+
   return (
-    <VStack className="flex-1 bg-brand-gray-900">
+    <VStack className="flex-1 bg-background-100">
       <ScreenHeader title="Histórico de exercícios" />
 
-      <HistoryCard />
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={() => <HistoryCard />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Heading>{title}</Heading>
+        )}
+        style={{ paddingHorizontal: 32, marginTop: 16 }}
+        contentContainerStyle={
+          exercises.length === 0
+            ? { flex: 1, justifyContent: "center" }
+            : { gap: 16 }
+        }
+        ListEmptyComponent={<Heading>Nenhum exercício realizado</Heading>}
+        showsVerticalScrollIndicator={false}
+      />
     </VStack>
   );
 }
