8129ed367caf0d1406ed0499a3156fdd5b6b691b
Felipe Jung <felipemattoseu@gmail.com>
2025-04-05 14:47:02 -0400

    Implement Icon component for SVG icons; update HomeHeader to include LogOut icon and adjust layout for improved user experience. Add new icon utilities for better icon management.

--- components/home-header.tsx
+++ components/home-header.tsx
@@ -3,17 +3,26 @@ import { HStack } from "./ui/hstack";
 import { VStack } from "./ui/vstack";
 import { Text } from "./ui/text";
 
+import { LogOut } from "lucide-react-native";
+
 import { UserPhoto } from "./user-photo";
+import { Icon } from "./ui/icon";
 
 export function HomeHeader() {
   return (
     <HStack className="w-full items-center justify-between bg-brand-gray-600 pt-16 pb-5 px-8 gap-4">
-      <UserPhoto source={{ uri: "https://github.com/felipe-jm.png" }} />
+      <UserPhoto
+        source={{ uri: "https://github.com/felipe-jm.png" }}
+        className="w-16 h-16"
+        alt="Imagem do usuário"
+      />
 
-      <VStack>
+      <VStack className="flex-1">
         <Text className="text-gray-100 font-regular text-base">Olá,</Text>
         <Heading className="text-gray-200 font-bold text-base">João</Heading>
       </VStack>
+
+      <Icon as={LogOut} color="gray-200" size="xl" />
     </HStack>
   );
 }
