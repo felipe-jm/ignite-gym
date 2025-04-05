960a1805d85f88a7199afa5b5537c29b0006f4ad
Felipe Jung <felipemattoseu@gmail.com>
2025-04-05 15:17:15 -0400

    Update package dependencies for navigation and reanimated; add Card component for UI consistency; implement HistoryCard and integrate it into History screen for improved exercise history display.

--- components/home-header.tsx
+++ components/home-header.tsx
@@ -7,22 +7,25 @@ import { LogOut } from "lucide-react-native";
 
 import { UserPhoto } from "./user-photo";
 import { Icon } from "./ui/icon";
+import { Card } from "./ui/card";
 
 export function HomeHeader() {
   return (
-    <HStack className="w-full items-center justify-between bg-brand-gray-600 pt-16 pb-5 px-8 gap-4">
-      <UserPhoto
-        source={{ uri: "https://github.com/felipe-jm.png" }}
-        className="w-16 h-16"
-        alt="Imagem do usuário"
-      />
+    <Card variant="filled">
+      <HStack className="mt-10 pb-5 items-center gap-4">
+        <UserPhoto
+          source={{ uri: "https://github.com/felipe-jm.png" }}
+          className="w-16 h-16"
+          alt="Imagem do usuário"
+        />
 
-      <VStack className="flex-1">
-        <Text className="text-gray-100 font-regular text-base">Olá,</Text>
-        <Heading className="text-gray-200 font-bold text-base">João</Heading>
-      </VStack>
+        <VStack className="flex-1">
+          <Text>Olá,</Text>
+          <Heading>João</Heading>
+        </VStack>
 
-      <Icon as={LogOut} color="gray-200" size="xl" />
-    </HStack>
+        <Icon as={LogOut} color="gray-200" size="xl" />
+      </HStack>
+    </Card>
   );
 }
--- screens/history.tsx
+++ screens/history.tsx
@@ -1,10 +1,12 @@
 import { VStack } from "@/components/ui/vstack";
 import { ScreenHeader } from "@/components/screen-header";
-
+import { HistoryCard } from "@/components/history-card";
 export function History() {
   return (
     <VStack className="flex-1 bg-brand-gray-900">
       <ScreenHeader title="Histórico de exercícios" />
+
+      <HistoryCard />
     </VStack>
   );
 }
