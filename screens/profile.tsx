922f54e4669e7506a5e6b86f69d174793751405e
Felipe Jung <felipemattoseu@gmail.com>
2025-04-06 13:55:14 -0400

    Implement profile form with validation using react-hook-form and zod. Add user photo handling and update functionality, enhancing user experience in the Profile screen.

--- screens/profile.tsx
+++ screens/profile.tsx
@@ -3,6 +3,11 @@ import { ScrollView, TouchableOpacity } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import * as FileSystem from "expo-file-system";
 
+import { Controller, useForm } from "react-hook-form";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { VStack } from "@components/ui/vstack";
 import { Center } from "@components/ui/center";
 import { Text } from "@components/ui/text";
@@ -17,10 +22,74 @@ import { ToastMessage } from "@components/toast-message";
 
 import defaultUserPhotoImg from "@assets/userPhotoDefault.png";
 
-export function Profile() {
-  const [userPhoto, setUserPhoto] = useState(defaultUserPhotoImg);
+import { useAuth } from "@hooks/useAuth";
+import { api } from "@services/api";
 
+type FormDataProps = {
+  name: string;
+  email: string;
+  password?: string;
+  old_password?: string;
+  confirm_password?: string;
+};
+
+const profileSchema = z
+  .object({
+    name: z.string().min(1, "Nome é obrigatório"),
+    email: z.string().email("Formato de email inválido"),
+    password: z
+      .string()
+      .min(6, "A senha deve ter pelo menos 6 caracteres")
+      .optional()
+      .or(z.literal("")),
+    old_password: z
+      .string()
+      .min(6, "A senha deve ter pelo menos 6 caracteres")
+      .optional()
+      .or(z.literal("")),
+    confirm_password: z
+      .string()
+      .min(6, "A senha deve ter pelo menos 6 caracteres")
+      .optional()
+      .or(z.literal("")),
+  })
+  .refine((data) => !data.password || data.password === data.confirm_password, {
+    message: "As senhas não coincidem",
+    path: ["confirm_password"],
+  })
+  .refine(
+    (data) => {
+      return !data.password || (!!data.password && !!data.old_password);
+    },
+    {
+      message: "Informe a senha antiga para definir uma nova senha",
+      path: ["old_password"],
+    }
+  );
+
+export function Profile() {
   const toast = useToast();
+
+  const { user } = useAuth();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>({
+    mode: "onChange",
+    defaultValues: {
+      name: user.name,
+      email: user.email,
+    },
+    resolver: zodResolver(profileSchema),
+  });
+
+  const [userPhoto, setUserPhoto] = useState(
+    user.avatar
+      ? { uri: `${api.defaults.baseURL}/avatar/${user.avatar}` }
+      : defaultUserPhotoImg
+  );
 
   async function handleUserPhotoSelect() {
     try {
@@ -63,6 +132,10 @@ export function Profile() {
     }
   }
 
+  async function handleProfileUpdate(data: FormDataProps) {
+    console.log(data);
+  }
+
   return (
     <VStack className="flex-1 bg-brand-gray-900">
       <ScreenHeader title="Perfil" />
@@ -84,23 +157,32 @@ export function Profile() {
           </TouchableOpacity>
 
           <Center className="w-full mt-6 gap-4">
-            <Input
-              placeholder="Nome"
-              onChangeText={() => {}}
-              className="bg-brand-gray-600"
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Nome"
+                  onChangeText={onChange}
+                  value={value}
+                  className="bg-brand-gray-600"
+                  errorMessage={errors.name?.message}
+                />
+              )}
             />
 
-            <Input
-              placeholder="Email"
-              className="bg-brand-gray-600"
-              isReadOnly
-              onChangeText={() => {}}
-            />
-
-            <Input
-              placeholder="Senha"
-              className="bg-brand-gray-600"
-              onChangeText={() => {}}
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Email"
+                  className="bg-brand-gray-600"
+                  isReadOnly
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
             />
           </Center>
 
@@ -109,25 +191,52 @@ export function Profile() {
           </Heading>
 
           <Center className="w-full gap-4">
-            <Input
-              placeholder="Senha antiga"
-              className="bg-brand-gray-600"
-              onChangeText={() => {}}
+            <Controller
+              control={control}
+              name="old_password"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Senha antiga"
+                  className="bg-brand-gray-600"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.old_password?.message}
+                />
+              )}
             />
 
-            <Input
-              placeholder="Nova senha"
-              className="bg-brand-gray-600"
-              onChangeText={() => {}}
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Senha"
+                  className="bg-brand-gray-600"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.password?.message}
+                />
+              )}
             />
 
-            <Input
-              placeholder="Confirmar nova senha"
-              className="bg-brand-gray-600"
-              onChangeText={() => {}}
+            <Controller
+              control={control}
+              name="confirm_password"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Confirmar nova senha"
+                  className="bg-brand-gray-600"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.confirm_password?.message}
+                />
+              )}
             />
 
-            <Button title="Atualizar" />
+            <Button
+              title="Atualizar"
+              onPress={handleSubmit(handleProfileUpdate)}
+            />
           </Center>
         </Center>
       </ScrollView>
