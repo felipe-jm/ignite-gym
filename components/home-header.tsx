e132702e512cf9299e7d0b0c2ad41a71e32c3545
Felipe Jung <felipemattoseu@gmail.com>
2025-04-06 12:02:40 -0400

    Update HomeHeader component to utilize user data from authentication context, displaying user's avatar and name. Implement signOut functionality on button press, enhancing user experience and personalization.

--- components/home-header.tsx
+++ components/home-header.tsx
@@ -1,3 +1,5 @@
+import { LogOut } from "lucide-react-native";
+
 import { Heading } from "./ui/heading";
 import { HStack } from "./ui/hstack";
 import { Text } from "./ui/text";
@@ -5,24 +7,30 @@ import { VStack } from "./ui/vstack";
 import { UserPhoto } from "./user-photo";
 import { Button, ButtonIcon } from "./ui/button";
 
-import { LogOut } from "lucide-react-native";
-
 import defaultUserPhotoImg from "@assets/userPhotoDefault.png";
 
+import { useAuth } from "@routes/hooks/useAuth";
+
 export function HomeHeader() {
+  const { user, signOut } = useAuth();
+
   return (
     <HStack className="bg-brand-gray-600 pt-16 pb-5 px-8 items-center gap-4">
-      <UserPhoto source={defaultUserPhotoImg} alt="Foto do usuário" size="sm" />
+      <UserPhoto
+        source={user.avatar ? { uri: user.avatar } : defaultUserPhotoImg}
+        alt="Foto do usuário"
+        size="sm"
+      />
       <VStack className="flex-1">
         <Text className="text-brand-gray-100">Olá,</Text>
-        <Heading className="text-brand-gray-100">John Doe</Heading>
+        <Heading className="text-brand-gray-100">{user.name}</Heading>
       </VStack>
 
       <Button
         variant="link"
         size="xl"
         className="justify-start items-center"
-        onPress={() => {}}
+        onPress={() => signOut()}
       >
         <ButtonIcon size="xl" as={LogOut} color="#C4C4CC" />
       </Button>
